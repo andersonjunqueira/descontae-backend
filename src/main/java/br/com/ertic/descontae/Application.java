8d4f848b40a2629dd0e8c7a717234aff25e163d9
acosorio <anderson.junqueira@gmail.com>
2017-06-04 21:03:37 -0300

    criando os primeiros serviços de testes

--- src/main/java/br/com/ertic/descontae/Application.java
+++ src/main/java/br/com/ertic/descontae/Application.java
@@ -1,17 +1,27 @@
 package br.com.ertic.descontae;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
-public class DescontaeApplication {
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages={
+    "br.com.ertic.descontae.interfaces.web",
+    "br.com.ertic.descontae.domain.service",
+    "br.com.ertic.descontae.infraestructure.persistence.jpa",
+    "br.com.ertic.util.infraestructure.web",
+    "br.com.ertic.util.infraestructure.service"
+})
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DescontaeApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
