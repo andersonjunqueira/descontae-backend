a3d1a3943e41f4d6ca65721ed19a3fe7d39e7357
Anderson Junqueira <acosorio@stefanini.com>
2017-06-13 14:37:52 -0300

    all origins

--- src/main/java/br/com/ertic/descontae/Application.java
+++ src/main/java/br/com/ertic/descontae/Application.java
@@ -29,7 +29,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
