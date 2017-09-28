867d422866a77d53fab97fab7399d50a782cd13a
acosorio <anderson.junqueira@gmail.com>
2017-09-27 23:08:43 -0300

    ok

--- src/main/java/br/com/ertic/descontae/interfaces/web/ConsumosController.java
+++ src/main/java/br/com/ertic/descontae/interfaces/web/ConsumosController.java
@@ -1,14 +1,11 @@
 package br.com.ertic.descontae.interfaces.web;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +16,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import br.com.ertic.descontae.domain.model.Consumo;
 import br.com.ertic.descontae.domain.service.ConsumoService;
 import br.com.ertic.descontae.interfaces.web.dto.ConsumoDTO;
-import br.com.ertic.descontae.interfaces.web.dto.ConsumoUsuarioDTO;
 import br.com.ertic.util.infraestructure.dto.Token;
 import br.com.ertic.util.infraestructure.exception.NegocioException;
 import br.com.ertic.util.infraestructure.log.Log;
@@ -72,27 +68,6 @@ public class ConsumosController extends RestFullEndpoint<Consumo, Long> {
            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
        }
    }
-
-    @RequestMapping(method = RequestMethod.GET, path="/pessoa/{id}")
-    public ResponseEntity<?> findConsumosUsuario(@PathVariable Long id) {
-        try {
-
-            List<ConsumoUsuarioDTO> saida = ((ConsumoService)service).findConsumosUsuario(id);
-            if(saida == null || saida.size() == 0) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            } else {
-                return new ResponseEntity<>(saida, HttpStatus.OK);
-            }
-
-        } catch (NegocioException ex) {
-            Log.error(this.getClass(), ex);
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
-
-        } catch (Exception ex) {
-            Log.error(this.getClass(), ex);
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
 }
 
--- src/main/java/br/com/ertic/descontae/interfaces/web/PessoasController.java
+++ src/main/java/br/com/ertic/descontae/interfaces/web/PessoasController.java
@@ -1,16 +1,22 @@
 package br.com.ertic.descontae.interfaces.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ertic.descontae.domain.model.Pessoa;
+import br.com.ertic.descontae.domain.service.ConsumoService;
 import br.com.ertic.descontae.domain.service.PessoaService;
+import br.com.ertic.descontae.interfaces.web.dto.ConsumoUsuarioDTO;
+import br.com.ertic.util.infraestructure.exception.NegocioException;
 import br.com.ertic.util.infraestructure.log.Log;
 import br.com.ertic.util.infraestructure.web.RestFullEndpoint;
 
@@ -18,6 +24,9 @@ import br.com.ertic.util.infraestructure.web.RestFullEndpoint;
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/pessoas")
 public class PessoasController extends RestFullEndpoint<Pessoa, Long> {
+
+    @Autowired
+    private ConsumoService consumoService;
 
     @Autowired
     public PessoasController(PessoaService service) {
@@ -60,4 +69,24 @@ public class PessoasController extends RestFullEndpoint<Pessoa, Long> {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.GET, path="/{id}/consumos")
+    public ResponseEntity<?> findConsumosUsuario(@PathVariable Long id) {
+        try {
+
+            List<ConsumoUsuarioDTO> saida = consumoService.findConsumosUsuario(id);
+            if(saida == null || saida.size() == 0) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(saida, HttpStatus.OK);
+            }
+
+        } catch (NegocioException ex) {
+            Log.error(this.getClass(), ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+
+        } catch (Exception ex) {
+            Log.error(this.getClass(), ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
