ab5025eaf4c325446c4f04baee418e72209b16a9
acosorio <anderson.junqueira@gmail.com>
2017-10-11 21:09:55 -0300

    ok

--- src/main/java/br/com/ertic/descontae/interfaces/web/ServicoController.java
+++ src/main/java/br/com/ertic/descontae/interfaces/web/ServicoController.java
@@ -1,5 +1,6 @@
 package br.com.ertic.descontae.interfaces.web;
 
+import org.apache.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ public class ServicoController {
 
     @RequestMapping(value="/ativo", method = RequestMethod.GET)
     public ResponseEntity<?> ativo() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.SC_UNPROCESSABLE_ENTITY).build();
     }
 
 }
