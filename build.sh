0f06ed3598be8ff26f4aaf39e995daeb20b92167
Anderson Junqueira <anderson.junqueira@gmail.com>
2017-10-29 19:26:39 -0200

    ambientes

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /c/Desenv/projetos/springboot-utils
-mvn clean install
-cd /c/Desenv/projetos/descontae-backend
-mvn clean package -DskipTests
-scp /c/Desenv/projetos/descontae-backend/target/descontae-backend.jar root@213.136.69.6:/opt/bin/descontae-backend.jar
